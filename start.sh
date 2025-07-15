2fb49e99d642202f719c69b725e96f0a5eb2fc89
isya <cahisya@gmail.com>
2025-07-15 15:51:34 +0800

    generate APP_KEY on Railway if not set

--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-php artisan migrate --force
+# Generate app key (jika belum ada)
+if [ -z "$APP_KEY" ]; then
+  php artisan key:generate
+fi
+
+# Cache config
 php artisan config:cache
 php artisan route:cache
+
+# Jalankan Laravel
 php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
