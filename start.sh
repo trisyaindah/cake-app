06b8ca3d042943e3c131780d6a04c462a12a6c8e
isya <cahisya@gmail.com>
2025-07-15 16:01:46 +0800

    fix: setup Laravel for Railway deploy

--- start.sh
+++ start.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# Generate APP_KEY kalau belum ada
-if [ ! -f bootstrap/cache/config.php ]; then
-  php artisan key:generate
-fi
-
-# Jalankan migrate jika perlu
-php artisan migrate --force || true
-
-# Cache config dan route
+php artisan config:clear
 php artisan config:cache
 php artisan route:cache
+php artisan key:generate
+php artisan migrate --force || true
 
-# Jalankan Laravel pakai host & port yang diminta Railway
 php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
